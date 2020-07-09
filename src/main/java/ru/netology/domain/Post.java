238bebd8ed2954a386e272ea3b8aec1bd2d521ec
Ekaterina Hlebalova <khlebalova.e@yandex.ru>
2020-07-09 18:15:59 +0400

    added classes

--- src/main/java/ru/netology/domain/Post.java
+++ src/main/java/ru/netology/domain/Post.java
@@ -1,4 +1,19 @@
 package ru.netology.domain;
 
 public class Post {
+    private String id;
+    private String owner_id;
+    private CommentsInfo commentsInfo;
+    private Article article;
+    private boolean like;
+    private boolean friendsOnly;
+    private String date;
+    private String text;
+    private boolean canPin;
+    private boolean canDelete;
+    private boolean canEdit;
+    private boolean canPinned;
+    private boolean isFavorite;
+    private boolean repost;
+
 }
--- src/main/java/ru/netology/domain/PostManager.java
+++ src/main/java/ru/netology/domain/PostManager.java
@@ -1,4 +1,10 @@
 package ru.netology.domain;
 
 public class PostManager {
+    private Post[] posts;
+
+    public Post[] getPostsForFeed() {
+        // TODO: add logic
+        return null;
+    }
 }
